a980a101cb774adb72a893531b2597b258d62ba6
Danil <danilovchinnikov04@gmail.com>
2023-03-30 23:14:15 +0700

    start develop profile.js

--- styles/gStyle.js
+++ styles/gStyle.js
@@ -24,5 +24,38 @@ export const gStyle = StyleSheet.create({
         height: "5%",
         backgroundColor: "#FE6223",
       },
+      profileImg: {
+        borderRadius: 250,
+        width: 100,
+        height: 100
+        
+      },
+      profileContainer: {
+        flexDirection: 'row',
+        paddingHorizontal: "10%",
+        paddingVertical: "8%",
+        borderBottomWidth: 0.5,
+        paddingBottom: "20%",
+        backgroundColor: "#FFF"
+      },
+      profileNameAndNumberView: {
+        paddingHorizontal: "12%",
+        paddingVertical: "6%",
+      },
+      profileTextNameAndNumber: {
+        fontSize: 20,
+        fontFamily: 'roboto-500',
+        color: '#333138',
+      },
+      profileSettings: {
+        flexDirection: 'row',
+        paddingHorizontal: "10%",
+        paddingVertical: "5%",
+        paddingBottom: "5%",
+        backgroundColor: "#FFF"
+      },
+      profileSettingNameText: {
+        
+      }
 })
 
