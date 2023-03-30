e6a296d4a2df47e12ac860629de8bcb8185240a7
LezzIK <lubyshev.alexandr@yandex.ru>
2023-03-30 23:16:59 +0700

    main.js is done

--- styles/gStyle.js
+++ styles/gStyle.js
@@ -17,12 +17,59 @@ export const gStyle = StyleSheet.create({
         paddingVertical: 10,
         paddingHorizontal: "5%",
         borderBottomWidth: 1,
-        borderBottomColor: 'rgba(0, 0, 0, 0.1)'
+        borderBottomColor: 'rgba(0, 0, 0, 0.1)',
+        marginBottom: 10,
       },
       header: {
         width: "100%",
         height: "5%",
         backgroundColor: "#FE6223",
       },
+      restCard: {
+        paddingHorizontal: '5%',
+      },
+      mainRest: {
+        marginBottom: 15,
+      },
+      mainRestImg: {
+        width: '100%',
+        height: 200,
+        borderRadius: 10,
+      },
+      mainRestTitle: {
+        fontFamily: 'roboto-700',
+        fontSize: 22,
+        color: '#333138',
+      },
+      mainRestDescr: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      },
+      mainRestNavText: {
+        fontFamily: 'roboto-400',
+        fontSize: 14, 
+        color: '#333138',
+        paddingLeft: 10, 
+      },
+      mainRestNav: {
+        flexDirection: 'row',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 10,
+        backgroundColor: "#F8F9FB",
+      },
+      mainRestStar: {
+        flexDirection: 'row',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 10,
+        backgroundColor: "#F8F9FB",
+      },
+      mainRestStarText: {
+        marginRight: 8,
+        fontFamily: 'roboto-400',
+        fontSize: 14, 
+        color: '#333138',
+      }
 })
 
--- сomponents/main.js
+++ сomponents/main.js
@@ -5,15 +5,17 @@ import {
   View,
   TouchableOpacity,
   FlatList,
+  Image,
 } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
 import { Feather } from "@expo/vector-icons";
 import { gStyle } from "../styles/gStyle";
 
-export default function Main({navigation}) {
+export default function Main({ navigation }) {
   const [info, setInfo] = useState([
     {
-      img: "../img/rest1.jpg",
+      img: "https://klike.net/uploads/posts/2022-09/1662641376_a.jpg",
       title: "Madagascar",
       descr:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -21,7 +23,7 @@ export default function Main({navigation}) {
       stars: "4,9",
     },
     {
-      img: "../img/rest2.jpg",
+      img: "https://tataevfamily.ru/wp-content/uploads/2022/06/12-2880x1920.jpg",
       title: "Racsagadam",
       descr:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -29,7 +31,7 @@ export default function Main({navigation}) {
       stars: "9.4",
     },
     {
-      img: "../img/rest3.jpg",
+      img: "https://static.tildacdn.com/tild6639-6536-4962-a131-373562346337/RUSKI_interior_5.JPG",
       title: "MamaRoma",
       descr: "Ну давай, залезай сюда",
       nav: "ул. Пушкина, дом 2",
@@ -45,22 +47,28 @@ export default function Main({navigation}) {
         <Feather name="list" size={24} color="black" />
       </View>
       <FlatList
+        style={gStyle.restCard}
         data={info}
-        renderItem={({ item }) => {
-          <TouchableOpacity
-            onPress={() => navigator.navigate("restInfo", item)}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={gStyle.mainRest}
+            onPress={() => navigation.navigate("fullInfo", item)}
           >
             <View>
-              <Text>{item.title}</Text>
-              <View>
-                <Text>{item.nav}</Text>
-                <View>
-                  <Text>{item.stars}</Text>
+              <Image style={gStyle.mainRestImg} source={{ uri: item.img }} />
+              <Text style={gStyle.mainRestTitle}>{item.title}</Text>
+              <View style={gStyle.mainRestDescr}>
+                <View style={gStyle.mainRestNav}>
+                  <Ionicons name="location-outline" size={20} color="black" />
+                  <Text style={gStyle.mainRestNavText}>{item.nav}</Text>
+                </View>
+                <View style={gStyle.mainRestStar}>
+                  <Text style={gStyle.mainRestStarText}>{item.stars}</Text>
+                  <Ionicons name="star" size={18} color="black" />
                 </View>
               </View>
             </View>
-          </TouchableOpacity>;
-        }}
+          </TouchableOpacity>
+        )}
       />
     </View>
   );
